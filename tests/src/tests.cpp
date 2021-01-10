e19ff9fc6365d53b4142e798e70834a4b275e315
Grzybiarz47 <j.zajda47@gmail.com>
2021-01-10 02:05:23 +0100

    Test update

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -2,6 +2,12 @@
 #include "gmock/gmock.h"
 #include "CircularBufferTests.h"
 
+/*******
+ * Repozytorium:
+ * https://github.com/Grzybiarz47/ADV-TDD-Projekt
+ * 
+********/
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
