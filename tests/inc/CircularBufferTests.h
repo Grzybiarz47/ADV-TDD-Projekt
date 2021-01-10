f45e4b24ae3c164f255ccdbb0188810b9de0b54f
Grzybiarz47 <j.zajda47@gmail.com>
2021-01-10 02:01:40 +0100

    Upload tests

--- tests/inc/CircularBufferTests.h
+++ tests/inc/CircularBufferTests.h
@@ -2,5 +2,68 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#define private public
+#include "CircularBuffer.h"
+
+class CircularBufferTests : public ::testing::Test 
+{
+public:
+	CircularBuffer<double>* inc;
+
+	void SetUp() override 
+    {
+		inc = new CircularBuffer<double>(3);
+        inc->add(2.1);
+        inc->add(-1);
+	}
+	void TearDown() override 
+    {
+		delete inc;
+	}
+};
+
+TEST_F(CircularBufferTests, MaxSize)
+{
+    EXPECT_EQ(inc->getAllocatedSize(), 3);
+}
+TEST_F(CircularBufferTests, RemoveAndSize)
+{
+    inc->remove();
+    inc->remove();
+    EXPECT_EQ(inc->getSize(), 0);
+}
+TEST_F(CircularBufferTests, AddAndSize)
+{
+    inc->add(3.12);
+    inc->add(-2.56);
+    EXPECT_EQ(inc->getSize(), 3);
+}
+TEST_F(CircularBufferTests, AddAndRemove)
+{
+    inc->add(3.12);
+    inc->add(-2.56);
+    EXPECT_EQ(inc->remove(), -1);
+}
+TEST_F(CircularBufferTests, Remove1)
+{
+    EXPECT_DOUBLE_EQ(inc->remove(), 2.1);
+}
+TEST_F(CircularBufferTests, Remove2)
+{
+    inc->remove();
+    EXPECT_DOUBLE_EQ(inc->remove(), -1);
+}
+TEST_F(CircularBufferTests, Remove3)
+{
+    inc->remove();
+    inc->remove();
+    EXPECT_THROW(inc->remove(), std::exception);
+}
+TEST_F(CircularBufferTests, IncrementIndex)
+{
+    int temp = 2;
+    inc->incrementIndex(temp);
+    EXPECT_EQ(temp, 0);
+}
 
 
